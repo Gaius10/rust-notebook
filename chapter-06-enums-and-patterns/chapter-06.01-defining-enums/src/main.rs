1d8d09ddc616e0d4fd24f63f687761f94045b81c
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-19 12:51:51 -0300

    Associating more complex types

--- chapter-06-enums-and-patterns/chapter-06.01-defining-enums/src/main.rs
+++ chapter-06-enums-and-patterns/chapter-06.01-defining-enums/src/main.rs
@@ -1,13 +1,21 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
+struct Ipv4Addr {
+    // --snip--
+}
+
+struct Ipv6Addr {
+    // --snip--
+}
+
 enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    let home = IpAddr::V4(Ipv4Addr {});
+    let loopback = IpAddr::V6(Ipv6Addr {});
 }
 
