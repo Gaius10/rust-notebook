f87e3dcb0a9243d76fefa6bba6c8a7fb1e9750ce
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-18 23:25:14 -0300

    Example program: debugging

--- chapter-05-structs/chapter-05.02-example-program/src/main.rs
+++ chapter-05-structs/chapter-05.02-example-program/src/main.rs
@@ -1,11 +1,14 @@
+#![allow(unused_variables)]
+#[derive(Debug)] // Makes Rectangle printable with :? specifier
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale), // dbg! returns ownership of value
         height: 50,
     };
 
@@ -13,6 +16,13 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+
+
+    println!("rect1 is {rect1:?}");
+    println!("rect1 is {rect1:#?}");
+
+    let rect1 = dbg!(&rect1); // Passing reference to avoid giving
+                              // ownership to dbg! macro.
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
