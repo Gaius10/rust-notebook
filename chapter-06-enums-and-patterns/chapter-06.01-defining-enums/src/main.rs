b7acc641b8db9a4101cde6c59444f9f4fa0999aa
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-19 13:02:22 -0300

    Enum methods

--- chapter-06-enums-and-patterns/chapter-06.01-defining-enums/src/main.rs
+++ chapter-06-enums-and-patterns/chapter-06.01-defining-enums/src/main.rs
@@ -14,8 +14,28 @@ enum IpAddr {
     V6(Ipv6Addr),
 }
 
+//
+// This is similar to defining multiple different structs,
+// but here they're grouped, so we could write a function that
+// accepts anyone of them as argument.
+//
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // do something...
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(Ipv4Addr {});
     let loopback = IpAddr::V6(Ipv6Addr {});
+    let m = Message::Write(String::from("Hello"));
+    m.call();
 }
 
