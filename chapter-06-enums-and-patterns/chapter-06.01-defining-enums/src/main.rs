13a613ae977af2e17616dc9a5020ab6df2c84b3b
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-19 12:49:11 -0300

    Associating more info with enum

--- chapter-06-enums-and-patterns/chapter-06.01-defining-enums/src/main.rs
+++ chapter-06-enums-and-patterns/chapter-06.01-defining-enums/src/main.rs
@@ -1,28 +1,13 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-}
-
-fn route(ip_kind: IpAddrKind) {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
 }
 
