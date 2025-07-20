958aa27d77ce00a838650e882d69a63e66b83943
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-19 21:07:38 -0300

    Example with Option<T>

--- chapter-06-enums-and-patterns/chapter-06.02-match/src/main.rs
+++ chapter-06-enums-and-patterns/chapter-06.02-match/src/main.rs
@@ -12,11 +12,23 @@ enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter(UsState), // Different value for each state
+    Quarter(UsState), // Different coin for each state
 }
 
 fn main() {
-    println!("Hello, world!");
+    let value = value_in_cents(Coin::Penny);
+
+    // With the Option<T>...
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
