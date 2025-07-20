3dcb532201256d502125675ccd786595d4029cc6
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-19 21:32:27 -0300

    Catch-all patterns

--- chapter-06-enums-and-patterns/chapter-06.02-match/src/main.rs
+++ chapter-06-enums-and-patterns/chapter-06.02-match/src/main.rs
@@ -22,6 +22,24 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+
+    //
+    // Obs.: matches MUST deal with all possible cases, so
+    // commenting the None => None, line would crash the program
+    // at compile time.
+    //
+    // For that, there are catch all patterns and the _ placeholder
+    //
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+
+        // Another possibilities ignoring the value
+        // _ => reroll()
+        // _ => () // Nothing happens, resolve to unit value
+    }
 }
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
@@ -42,4 +60,8 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
     }
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
 
