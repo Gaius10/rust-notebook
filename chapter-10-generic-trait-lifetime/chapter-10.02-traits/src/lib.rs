e98c3498a97464e84de5b2eacbe58c5c4eef15a4
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-25 02:30:33 -0300

    Granular specifications

--- chapter-10-generic-trait-lifetime/chapter-10.02-traits/src/lib.rs
+++ chapter-10-generic-trait-lifetime/chapter-10.02-traits/src/lib.rs
@@ -1,7 +1,12 @@
 
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        format!(
+            "(Read more from {}...)",
+            self.summarize_author(),
+        )
     }
 }
 
@@ -12,7 +17,11 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-impl Summary for NewsArticle {}
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        String::from("some great author")
+    }
+}
 
 pub struct SocialPost {
     pub username: String,
@@ -22,6 +31,10 @@ pub struct SocialPost {
 }
 
 impl Summary for SocialPost {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
