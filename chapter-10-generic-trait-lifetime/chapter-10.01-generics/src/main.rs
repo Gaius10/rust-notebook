e5771ff4e1ec756059fde92a68ba446358676465
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-25 01:10:51 -0300

    DRY using generics

--- chapter-10-generic-trait-lifetime/chapter-10.01-generics/src/main.rs
+++ chapter-10-generic-trait-lifetime/chapter-10.01-generics/src/main.rs
@@ -1,16 +1,6 @@
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
+use std::cmp::PartialOrd;
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -26,10 +16,10 @@ fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {result}");
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {result}");
 }
 
