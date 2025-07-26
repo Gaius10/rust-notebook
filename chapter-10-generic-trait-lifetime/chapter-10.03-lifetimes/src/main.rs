db7ae6a7856ab3b26d1a89cca1c5197192b506e6
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-26 20:16:23 -0300

    Just one more complicated example

--- chapter-10-generic-trait-lifetime/chapter-10.03-lifetimes/src/main.rs
+++ chapter-10-generic-trait-lifetime/chapter-10.03-lifetimes/src/main.rs
@@ -85,3 +85,17 @@ fn main() {
 //  rules come up, as more patterns are discovered.
 ////////////////////////////////////////////////////////////////////
 
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T> (
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() { x } else { y }
+}
+
