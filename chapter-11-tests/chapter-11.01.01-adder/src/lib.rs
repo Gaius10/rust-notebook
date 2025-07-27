c05da40855ed8c5683d37a48c7422f446f224880
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-27 16:01:43 -0300

    Rectangle tests

--- chapter-11-tests/chapter-11.01.01-adder/src/lib.rs
+++ chapter-11-tests/chapter-11.01.01-adder/src/lib.rs
@@ -1,5 +1,21 @@
+#![allow(dead_code)]
+#![allow(unused_variables)];
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width &&
+        self.height > other.height
+    }
 }
 
 #[cfg(test)]
@@ -13,7 +29,34 @@ mod tests {
     }
 
     #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+
     }
 }
+
