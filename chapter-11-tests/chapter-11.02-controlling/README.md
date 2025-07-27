7c6c77fbbd3f11185cd2452c1d464e18bbd85efb
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-27 18:13:03 -0300

    Some useful flags to cargo test

---
+++ chapter-11-tests/chapter-11.02-controlling/README.md
@@ -0,0 +1,28 @@
+# Controlling test workflow
+
+## General syntax:
+
+`cargo test <cargo_test_flags> -- <flags_of_created_binary>`
+
+## Running tests syncronously:
+
+`cargo test -- --test-threads=1`
+
+## Outputs:
+
+By default, Rust shows only failed tests outputs.
+We can change it with: `cargo test -- --show-output`
+
+## Running single tests:
+
+`cargo test <test_name>`
+try `cargo test one_hundred` in this repository
+
+## Filtering tests:
+
+try `cargo test add`
+
+## Running ignored tests:
+
+`cargo test -- --ignored`
+
---
+++ chapter-11-tests/chapter-11.02-controlling/src/lib.rs
@@ -0,0 +1,52 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {a}");
+    10
+}
+
+fn add_two(a: usize) -> usize {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(value, 10);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(value, 5);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn one_hundred() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
+    }
+}
