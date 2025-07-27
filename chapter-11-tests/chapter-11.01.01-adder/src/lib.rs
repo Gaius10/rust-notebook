74b6fc42ad24f7acfdf68171930110c0f340bc45
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-27 16:28:43 -0300

    Should panic example

--- chapter-11-tests/chapter-11.01.01-adder/src/lib.rs
+++ chapter-11-tests/chapter-11.01.01-adder/src/lib.rs
@@ -26,6 +26,20 @@ impl Rectangle {
     }
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 and 100, got {value}.");
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,6 +93,12 @@ mod tests {
         };
 
         assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
 
