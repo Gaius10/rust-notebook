7d2e4a40781c51f987dc99cf66256023feff1731
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-28 16:25:02 -0300

    Added search functionality to the core::run() - project completed

--- chapter-12-grep/src/core.rs
+++ chapter-12-grep/src/core.rs
@@ -5,7 +5,9 @@ use crate::config::Config;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
