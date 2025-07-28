69cd5ea395b3f3f1a020973c79b04161b9ca816b
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-28 16:19:03 -0300

    Search function implementation

--- chapter-12-grep/src/core.rs
+++ chapter-12-grep/src/core.rs
@@ -10,3 +10,34 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matches: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            matches.push(line);
+        }
+    }
+
+    matches
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        let expected = vec!["safe, fast, productive."];
+        let actual = search(query, contents);
+
+        assert_eq!(expected, actual);
+    }
+}
+
