31bee613833b2b196333007251ee47d5c0364b58
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-28 14:54:33 -0300

    Move core code to src/lib.rs

---
+++ chapter-12-grep/src/lib.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Self, String> {
+        if args.len() < 3 {
+            return Err(format!(
+                "Not enough arguments.\n\
+                Usage: {} <search_string> <file_path>",
+                args[0]
+            ));
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
--- chapter-12-grep/src/main.rs
+++ chapter-12-grep/src/main.rs
@@ -1,6 +1,8 @@
-use std::{env, fs, process};
-use std::error::Error;
+use std::env;
+use std::process;
 
+use minigrep::Config;
+;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
@@ -11,38 +13,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run (config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, String> {
-        if args.len() < 3 {
-            return Err(format!(
-                "Not enough arguments.\n\
-                Usage: {} <search_string> <file_path>",
-                args[0]
-            ));
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }
 
