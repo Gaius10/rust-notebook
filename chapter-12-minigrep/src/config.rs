fa5f7933b14b9a34710fdb6c58848d2274b4d059
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-07-31 21:41:37 -0300

    Chapter 13.03 - moving ownership of iterator

--- chapter-12-minigrep/src/config.rs
+++ chapter-12-minigrep/src/config.rs
@@ -7,20 +7,28 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Self, String> {
-        if args.len() < 3 {
-            return Err(format!(
-                "Not enough arguments.\n\
-                Usage: {} <search_string> <file_path>",
-                args[0]
-            ));
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Self, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case
+        })
     }
 }
 
