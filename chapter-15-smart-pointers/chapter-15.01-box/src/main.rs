28695d5b658b1eeb502c16a727586cf1c9730a0f
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-08-10 18:11:17 -0300

    Implementation of cons lists using Box<T>

--- chapter-15-smart-pointers/chapter-15.01-box/src/main.rs
+++ chapter-15-smart-pointers/chapter-15.01-box/src/main.rs
@@ -1,4 +1,20 @@
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(dead_code)]
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
+    // Box abstraction provides only the indirection of keeping
+    // data on the heap instead of the stack, nothing more than
+    // that.
     let b = Box::new(5);
     println!("b = {b}");
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
