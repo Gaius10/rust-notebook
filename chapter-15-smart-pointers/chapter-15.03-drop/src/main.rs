d31c6b5ff87fac8d9fd31929ad2b18031ba8e3eb
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-08-12 11:10:20 -0300

    Simple example about how Drop trait works

---
+++ chapter-15-smart-pointers/chapter-15.03-drop/src/main.rs
@@ -0,0 +1,28 @@
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(dead_code)]
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!(
+            "Dropping CustomSmartPointer with data `{}`!",
+            self.data
+        );
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointers created.");
+}
